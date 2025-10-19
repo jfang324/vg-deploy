fba67a25ff126f6ccd70c960f7cdf1bea319130b
Jeffery Fang <jefferyfang324@gmail.com>
2025-10-19 01:38:46 -0400

    initial commit - created basic docker compose script, deploy script, and Caddyfile with a temporary domain

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e  # exit on any error
+
+# Go to project directory
+cd ~/vg-deploy
+
+# Fetch latest changes
+git fetch origin main
+
+echo "Pulling latest container images..."
+sudo docker-compose pull
+
+echo "Recreating containers with the latest images..."
+sudo docker-compose up -d
+
+echo "Cleaning up unused resources..."
+sudo docker image prune -f
+sudo docker container prune -f
